ccdd893e700f2f35881144c2eeff832d93c0adda
Hasnain <mrajhasnain101@gmail.com>
2022-06-21 19:54:54 +0600

    js

---
+++ js Dom practise/basicDom.js
@@ -0,0 +1,15 @@
+const sect=document.querySelector('section')
+const para=document.createElement('p');
+para.textContent="I am created By JS or ECMAscript12";
+sect.appendChild(para);
+const text = document.createTextNode(' — the premier source for web development knowledge.');
+const linkpara=document.querySelector('p');
+linkpara.appendChild(text);
+
+function addTextNode(text)
+{
+    const newtext=document.createTextNode(text);
+    const para=document.querySelector('p');
+
+    para.appendChild(newtext);
+}
