d6bae913493962236ba01bab150b788735998f30
Hasnain <mrajhasnain101@gmail.com>
2022-10-26 21:04:10 +0600

    states

--- reactjsbasics/tic-tac-toe/src/index.js
+++ reactjsbasics/tic-tac-toe/src/index.js
@@ -3,19 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
-    constructor(props){
+    constructor(props)
+    {
+        super(props)
         this.state={
-            value:null,
-        };
+            value:null
+        }
     }
     render() {
-        return (
-          <button className="square" onClick={() => console.log('click')}>
-            {this.props.value}
-          </button>
-        );
-      }
-     }
+      return (
+        <button className="square" onClick={()=>this.setState({value:'X'})
+        }>
+          {this.state.value}
+        </button>
+      );
+    }
+  }
   
   class Board extends React.Component {
     renderSquare(i) {
