dba48d7021f74c6a5e39a65e6f8ffba9177f285b
Hasnain <mrajhasnain101@gmail.com>
2022-10-27 21:24:42 +0600

    added pre

--- reactjsbasics/tic-tac-toe/src/index.js
+++ reactjsbasics/tic-tac-toe/src/index.js
@@ -21,9 +21,24 @@ class Square extends React.Component {
   }
   
   class Board extends React.Component {
-    renderSquare(i) {
-      return <Square value={i}/>;
-    }
+      constructor(props) {
+        super(props);
+        this.state = {
+          squares: Array(9).fill(null),
+        };
+      }
+
+      handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+      }
+      
+      renderSquare(i) {
+        return(<Square value={this.state.squares[i]} 
+        onClick={()=>this.handleClick(i)}
+        />) ;
+      }
   
     render() {
       const status = 'Next player: X';
