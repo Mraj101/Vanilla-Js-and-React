2ecdd75450320f28f93bf54afc50b42327340900
Hasnain <mrajhasnain101@gmail.com>
2022-10-31 18:47:14 +0600

    mixed variables

--- basic practise/dataType.js
+++ basic practise/dataType.js
@@ -2,4 +2,15 @@ const rafi='raf raf'
 const kafi='kaf kaf'
 var mil=rafi+' '+kafi
 var baro=12
-console.log(mil+baro)
+console.log(mil+baro);
+
+
+//string conversions
+
+var randString="abc";
+var number1=1;
+var number2=2;
+var containMixtypes1=number1+number2+randString
+console.log(containMixtypes1);
+var containMixtypes2=randString+number1+number2
+console.log(containMixtypes2);
