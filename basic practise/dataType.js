07ed70d4024c293b0df935caa0ffafd2e14c8782
Hasnain <mrajhasnain101@gmail.com>
2022-10-31 18:52:33 +0600

    string parsint int

--- basic practise/dataType.js
+++ basic practise/dataType.js
@@ -14,3 +14,18 @@ var containMixtypes1=number1+number2+randString
 console.log(containMixtypes1);
 var containMixtypes2=randString+number1+number2
 console.log(containMixtypes2);
+
+
+
+//concert string
+var myString = "56.02 degrees centigrade";
+ var myInt;
+ var myFloat;
+ console.log("\"" + myString + "\" is " + parseInt(myString, 10) +
+ " as an integer");
+ myInt = parseInt(myString, 10);
+ console.log("\"" + myString +
+ "\" when converted to an integer equals " + myInt);
+ myFloat = parseFloat(myString);
+ console.log("\"" + myString +
+ "\" when converted to a floating point number equals " + myFloat);
