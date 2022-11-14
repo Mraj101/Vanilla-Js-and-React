687dfce901d93e10bf2624b2bc962a39d02097e9
Hasnain <mrajhasnain101@gmail.com>
2022-11-14 22:07:29 +0600

    handle click inside game component

--- reactjsbasics/tic-tac-toe/src/index.js
+++ reactjsbasics/tic-tac-toe/src/index.js
@@ -11,17 +11,6 @@ import './index.css';
   }
   
   class Board extends React.Component {
-    handleClick(i) {
-      const squares = this.state.squares.slice();
-      if (calculateWinner(squares) || squares[i]) {
-        return;
-      }
-      squares[i] = this.state.xIsNext ? 'X' : 'O';
-      this.setState({
-        squares: squares,
-        xIsNext: !this.state.xIsNext,
-      });
-    }
   
     renderSquare(i) {
       return (
@@ -69,6 +58,22 @@ import './index.css';
         ]
       }
     }
+
+    handleClick(i) {
+      const history=this.state.history
+      const current=history[history.length-1];
+      const squares = current.squares.slice();
+      
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
+    }
+  
     render() {
       const history=this.state.history;
       const current=history[history.length-1];
