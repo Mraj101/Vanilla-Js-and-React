a1e5f151a28359877c49027e25b9ae12d5dbaf51
Hasnain <mrajhasnain101@gmail.com>
2022-11-16 23:53:46 +0600

    generated unique id using uuid

--- reactjsbasics/my-app/src/App.js
+++ reactjsbasics/my-app/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Card from "./component/Card"
 import Data from "./data.json"  
+import { v4 as uuidv4 } from "uuid";
      
 
- 
 
 
 
@@ -14,7 +14,7 @@ function App(){
        for(let i=0;i<Data.length;i++)
        items.push( <Card content={Data[i].content} title={Data[i].title}/> )    */
 
-       const item=Data.map((item,index) => <Card key={index} content={item.content} title={item.title}/>)
+       const item=Data.map((item,index) => <Card key={uuidv4()} content={item.content} title={item.title}/>)
   return (
     <div>
          {item}
