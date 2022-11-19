00834f8bd3f9505dd3d3e32e295207caca30ba19
Hasnain <mrajhasnain101@gmail.com>
2022-11-19 21:28:33 +0600

    react boot strap

---
+++ reactjsbasics/my-app/src/component/ReactBootstrap/ReactBootstrap.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+export default function ReactBootstrap() {
+  return (
+        <Card style={{width:"300px", margin:"auto"}}>
+           <Card.Body>
+             <Card.Title>Cards the freak</Card.Title>
+             <Card.Text>
+                hello , I am hasnain ahmed miraj, a bit old now but they say the peak of young age.
+             </Card.Text>
+             <Button>click</Button>
+           </Card.Body>
+        </Card>
+    )
+}
