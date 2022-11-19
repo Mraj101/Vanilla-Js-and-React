ac45b3108e1cb058f43c34e5575648f9ae940447
Hasnain <mrajhasnain101@gmail.com>
2022-11-19 19:30:48 +0600

    css added

--- reactjsbasics/my-app/src/component/Card.js
+++ reactjsbasics/my-app/src/component/Card.js
@@ -15,13 +15,21 @@ const footer={
     borderRadius:"8px",
     padding:"5px",
 }
+
+const styleContent={
+    padding:"10px",
+    lineHeight:"1.5",
+    fontSize:"18px",
+    fontWeight:"350",
+    textAlign:"justify"
+}
 function Card(props)
 {
     const {title,content}=props;
     return(
         <div className="Container">
             <h3 className="card-head">{title}</h3>
-            <p style={{padding:"10px",lineHeight:"1.5" ,fontSize:"18px",fontWeight:"350",textAlign:"justify"}}>
+            <p style={styleContent}>
                 {content}
             </p>
             <p style={footer}>{day+"/"+ month +"/"+ year}</p>
