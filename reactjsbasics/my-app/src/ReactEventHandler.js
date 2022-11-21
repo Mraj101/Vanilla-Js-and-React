51b8c58b59ca004445a9e48f89eea8a3edca51b5
Hasnain <mrajhasnain101@gmail.com>
2022-11-21 23:50:05 +0600

    error to resolve

--- reactjsbasics/my-app/src/ReactEventHandler.js
+++ reactjsbasics/my-app/src/ReactEventHandler.js
@@ -1,32 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-
-export default function HandleEvent() {
+export default class ReactEventHandler extends Component {
     constructor(props) {
       super(props)
     
       this.state = {
-         changedValue:''
+         changedValue:" "
       }
     }
-    const container={ 
-    border:"2px solid blue",
-    width:"500px",padding:"30px" ,
-    margin:"300px",
-    padding:"100px"
-   }
-   const onChangeHandler= (e)=>{
-    this.setState(
-        changedValue:this.e.target.value,
-    )
-   }
 
-  return (
-    <div style={container}>
+    const container={ 
+        border:"2px solid blue",
+        width:"500px",padding:"30px" ,
+        margin:"300px",
+        padding:"100px"
+       }
+
+ 
+       const onChangeHandler = (e)=>{
+           this.setState({
+               changedValue:e.target.value
+           } )
+      }
+          
+ render() {
+
+    return (
+        <div style={container}>
         <h1>
             Take an input plz
         </h1>
         <input onChange={this.onChangeHandler} type="text"/>
+        <p>
+            {this.state.changedValue}
+        </p>
     </div>
-  )
+    )
+  }
 }
+
+
+
+
+
+
