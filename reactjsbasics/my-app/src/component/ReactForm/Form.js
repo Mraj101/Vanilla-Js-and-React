170c7e7f768431efd41597c1862b224aa332fbaa
Hasnain <mrajhasnain101@gmail.com>
2022-11-23 22:55:14 +0600

    created basic form

--- reactjsbasics/my-app/src/component/ReactForm/Form.js
+++ reactjsbasics/my-app/src/component/ReactForm/Form.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import "./form.module.css"
+import style from "./form.module.css"
 const Form = () => {
     return (
-        <div  style={{height:"80vw" ,backgroundColor:"red" ,color:"black"}}>
-            <div className='Container' style={{ backgroundColor:"skyblue" ,color:"black"}}>
-                <h1>React Form</h1>
-                <div>
-                    <label htmlFor="name">Name</label>
+        <div  style={{height:"100vh" ,width:"100vw" ,color:"black"}}>
+            <div  className={style.containForm} style={{ backgroundColor:"skyblue" ,color:"black"}}>
+                <h1> React Form </h1>
+                <div className={style.formGroup} >
+                    <label htmlFor="name">Name: </label>
                     <input type="text" id='name' name='name'  />
                 </div>
                 
-                <div>
-                    <label htmlFor="email">Name</label>
-                    <input type="text" id='email' name='email'/>
+                <div className={style.formGroup}>
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" id='email' name='email'/>
                 </div>
+                
+                <div className={style.formGroup}>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id='password' name='password'/>
+                </div>
+                <button className={style.formGroup}>
+                    Register
+                </button>
 
             </div>
 
