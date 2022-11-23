6d137cc5faa60ca298d8f083aa8234dda33bf676
Hasnain <mrajhasnain101@gmail.com>
2022-11-23 22:35:16 +0600

    form module css

--- reactjsbasics/my-app/src/component/ReactForm/Form.js
+++ reactjsbasics/my-app/src/component/ReactForm/Form.js
@@ -1,17 +1,20 @@
 import React from 'react';
-
+import "./form.module.css"
 const Form = () => {
     return (
-        <div className='Container' style={{display:"block" ,backgroundColor:"skyblue" ,color:"black"}}>
-            <h1>React Form</h1>
-            <div>
-                <label htmlFor="name">Name</label>
-                <input type="text" id='name' name='name'  />
-            </div>
-            
-            <div>
-                <label htmlFor="email">Name</label>
-                <input type="text" id='email' name='email'/>
+        <div  style={{height:"80vw" ,backgroundColor:"red" ,color:"black"}}>
+            <div className='Container' style={{ backgroundColor:"skyblue" ,color:"black"}}>
+                <h1>React Form</h1>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id='name' name='name'  />
+                </div>
+                
+                <div>
+                    <label htmlFor="email">Name</label>
+                    <input type="text" id='email' name='email'/>
+                </div>
+
             </div>
 
         </div>
