2ea45d116830741a56055ebea4ba44f588908032
Hasnain <mrajhasnain101@gmail.com>
2022-11-25 18:16:16 +0600

    used useState passing object

--- reactjsbasics/my-app/src/component/ReactForm/Form.js
+++ reactjsbasics/my-app/src/component/ReactForm/Form.js
@@ -38,67 +38,64 @@ const ReactForm = () => {
 
 
 /////////another way of doing things.
-const [user,setUser]=useState({
-    name:'',
-    email:'',
-    password:''
-})
 
-const handleName=(e)=>{
-    setUser({
-        name:e.target.value,
-        email:'',
-        password:''
-    })
-}
 
-const handleEmail=(e)=>{
-    setUser({
-        name:'',
-        email:e.target.value,
-        password:'',
-    })
-}
-const handlePassword=(e)=>{
-    setUser({
-        name:'',
-        email:'',
-        password:e.target.value,
-    })
-}
 
-const handleSubmit=(e)=>{
-    let formInfo={
-        name:user.name,
-        email:user.email,
-        password:user.password
-    }
-    console.log(formInfo)
-    e.preventDefault()
-}
+// const [user,setUser]=useState({
+//  name:"",email:"",password:""
+// })
 
+// const {name, email, password} = user;
+
+// const handleName= (e) =>{
+//     setUser({ name:e.target.value, email, password})
+// }
+
+// const handleEmail= (e)=>{
+//     setUser({name, email:e.target.value, password})
+// }
+
+// const handlePassword= (e)=>{
+//     setUser({name, email, password:e.target.value})
+// }
+
+// const handleSubmit= (e)=>{
+//     console.log("form is submitted")
+//     console.log(user)
+//     e.preventDefault()
+// }
+
+
+
+
+
+
+///proper way is to define 1 handle change method and call it on on every input field and retrieve that data from there
+
+///
 
  
     return (
         <div  style={{height:"100vh" ,width:"100vw" ,color:"black",marginBottom:"50px"}}>
             <h1>The Data retrived from this FORM uses manual name setInfo for every input , which can be done by a single handler funcion </h1>
-            <form  action='' onSubmit={handleSubmit} className={style.containForm} style={{ backgroundColor:"skyblue" ,color:"black"}}>
+            <form  action='' onSubmit={handleSubmit} className={style.containForm} style={{      backgroundColor:"skyblue" ,color:"black"}}>
                 <h1> React Form </h1>
                 <div className={style.formGroup} >
                     <label htmlFor="name">Name: </label>
-                    <input onChange={handleName}  type="name" id='name' name='name'  required />
+                    <input onChange={handleName}  type="text" id='name' name='name' value={name}  required />
                 </div>
                 
                 <div className={style.formGroup}>
                     <label htmlFor="email">Email: </label>
-                    <input onChange={handleEmail} type="email" id='email' name='email' required/>
+                    <input onChange={handleEmail} type="email" id='email' name='email' value={email} required/>
                 </div>
                 
                 <div className={style.formGroup}>
                     <label htmlFor="password">Password:</label>
-                    <input onChange={handlePassword} type="password" id='password' name='password' required/>
+                    <input onChange={handlePassword} type="password" id='password' name='password' value={password} required/>
                 </div>
-                <button  className = {style.formGroup} >
+
+                <button type='submit'  className = {style.formGroup} >
                     Register
                 </button>
 
