30eaae5d7ff68dc146ad9f439646d20fb8322156
Hasnain <mrajhasnain101@gmail.com>
2022-11-28 00:36:18 +0600

    useFormik Hook is used

--- reactjsbasics/forms/src/App.js
+++ reactjsbasics/forms/src/App.js
@@ -1,37 +1,48 @@
-import React,{useState} from 'react';
+import React from 'react';
+import { useFormik } from 'formik';
 import style from './form.module.css'
 
 function App() {
 
-  const [user,setUser]=useState({name:"",email:"",password:"" })
-  const {name,email,password}=user
+  // const [user,setUser]=useState({name:"",email:"",password:"" })
+  // const {name,email,password}=user
 
-  const handleChange=(e) =>{
-   
-     setUser({...user, [e.target.name]:e.target.value})
- }
+  //I will use formik here
 
-  const handleSubmit=(e)=>{
-    console.log(user)
- }
+  const formik = useFormik({
+    initialValues:{
+      name:'',
+      email:'',
+      password:''
+    },
+    
+    onSubmit:(values,{resetForm})=>{
+      console.log(values)
+      resetForm({
+        values:""
+      })
+    },
+
+  })
+
 
   return (
     <div className="App">
-       <form  action='' onSubmit={handleSubmit} className={style.containForm} style={{      backgroundColor:"skyblue" ,color:"black"}}>
+       <form  action='' onSubmit={formik.handleSubmit} className={style.containForm} style={{      backgroundColor:"skyblue" ,color:"black"}}>
                 <h1> React Form </h1>
                 <div className={style.formGroup} >
                     <label htmlFor="name">Name: </label>
-                    <input onChange={handleChange}  type="text" id='name' name='name' value={name}  required />
+                    <input onChange={formik.handleChange}  type="text" id='name' name='name' value={formik.values.name}  required />
                 </div>
                 
                 <div className={style.formGroup}>
                     <label htmlFor="email">Email: </label>
-                    <input onChange={handleChange} type="email" id='email' name='email' value={email} required/>
+                    <input onChange={formik.handleChange} type="email" id='email' name='email' value={formik.values.email} required/>
                 </div>
                 
                 <div className={style.formGroup}>
                     <label htmlFor="password">Password:</label>
-                    <input onChange={handleChange} type="password" id='password' name='password' value={password} required/>
+                    <input onChange={formik.handleChange} type="password" id='password' name='password' value={formik.values.password} required/>
                 </div>
 
                 <button type='submit'  className = {style.formGroup} >
