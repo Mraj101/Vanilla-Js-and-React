4353e48de38f32abce295b8a58dc168fbfb11019
Hasnain <mrajhasnain101@gmail.com>
2022-11-28 21:27:12 +0600

    yup validationSchema

--- reactjsbasics/forms/src/App.js
+++ reactjsbasics/forms/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useFormik } from 'formik';
 import style from './form.module.css'
+import * as yup from 'yup';
+
+
+
 
 function App() {
 
@@ -15,9 +19,13 @@ function App() {
       email:'',
       password:''
     },
-    
+    validationSchema:yup.object({
+      name:yup.string().min(2,"must be 2 char long").required(),
+      email:yup.string().email().required(),
+      password:yup.string().min(6,"must be six characters").required(),
+    }),
+
     onSubmit:(values,{resetForm})=>{
-      console.log(values)
       resetForm({
         values:""
       })
@@ -33,16 +41,21 @@ function App() {
                 <div className={style.formGroup} >
                     <label htmlFor="name">Name: </label>
                     <input onChange={formik.handleChange}  type="text" id='name' name='name' value={formik.values.name}  required />
+                    {formik.touched.name && formik.errors.name && (<span>{formik.errors.name}</span>)}
                 </div>
                 
                 <div className={style.formGroup}>
                     <label htmlFor="email">Email: </label>
                     <input onChange={formik.handleChange} type="email" id='email' name='email' value={formik.values.email} required/>
+                    {formik.touched.email && formik.errors.name && (<span>{formik.errors.email}</span>)}
+
                 </div>
                 
                 <div className={style.formGroup}>
                     <label htmlFor="password">Password:</label>
                     <input onChange={formik.handleChange} type="password" id='password' name='password' value={formik.values.password} required/>
+                    {formik.touched.password && formik.errors.name && (<span>{formik.errors.password}</span>)}
+
                 </div>
 
                 <button type='submit'  className = {style.formGroup} >
