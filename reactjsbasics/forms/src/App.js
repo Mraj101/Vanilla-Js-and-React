3b744120f01ed87b95fb17d8045a6e98cb1f86fd
Hasnain <mrajhasnain101@gmail.com>
2022-11-29 22:47:13 +0600

    new components set up

--- reactjsbasics/forms/src/App.js
+++ reactjsbasics/forms/src/App.js
@@ -13,58 +13,71 @@ function App() {
 
   //I will use formik here
 
-  const formik = useFormik({
-    initialValues:{
-      name:'',
-      email:'',
-      password:''
-    },
-    validationSchema:yup.object({
-      name:yup.string().min(2,"must be 2 char long").required(),
-      email:yup.string().email().required(),
-      password:yup.string().min(6,"must be six characters").required(),
-    }),
+  // const formik = useFormik({
+  //   initialValues:{
+  //     name:'',
+  //     email:'',
+  //     password:''
+  //   },
+  //   validationSchema:yup.object({
+  //     name:yup.string().min(2,"must be 2 char long").required(),
+  //     email:yup.string().email().required(),
+  //     password:yup.string().min(6,"must be six characters").required(),
+  //   }),
 
-    onSubmit:(values,{resetForm})=>{
-      resetForm({
-        values:""
-      })
-    },
+  //   onSubmit:(values,{resetForm})=>{
+  //     resetForm({
+  //       values:""
+  //     })
+  //   },
 
   })
 
 
-  return (
-    <div className="App">
-       <form  action='' onSubmit={formik.handleSubmit} className={style.containForm} style={{      backgroundColor:"skyblue" ,color:"black"}}>
-                <h1> React Form </h1>
-                <div className={style.formGroup} >
-                    <label htmlFor="name">Name: </label>
-                    <input onChange={formik.handleChange}  type="text" id='name' name='name' value={formik.values.name}  required /><br/>
-                    {formik.touched.name && formik.errors.name && (<span style={{color:"red"}} >{formik.errors.name}</span>)}
-                </div>
+  return ( 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // <div className="App">
+    //    <form  action='' onSubmit={formik.handleSubmit} className={style.containForm} style={{      backgroundColor:"skyblue" ,color:"black"}}>
+    //             <h1> React Form </h1>
+    //             <div className={style.formGroup} >
+    //                 <label htmlFor="name">Name: </label>
+    //                 <input onChange={formik.handleChange}  type="text" id='name' name='name' value={formik.values.name}  required /><br/>
+    //                 {formik.touched.name && formik.errors.name && (<span style={{color:"red"}} >{formik.errors.name}</span>)}
+    //             </div>
                 
-                <div className={style.formGroup}>
-                    <label htmlFor="email">Email: </label>
-                    <input onChange={formik.handleChange} type="email" id='email' name='email' value={formik.values.email} required/>
-                    <br/>
-                    {formik.touched.email && formik.errors.name && (<span style={{color:"red"}}>{formik.errors.email}</span>)}
+    //             <div className={style.formGroup}>
+    //                 <label htmlFor="email">Email: </label>
+    //                 <input onChange={formik.handleChange} type="email" id='email' name='email' value={formik.values.email} required/>
+    //                 <br/>
+    //                 {formik.touched.email && formik.errors.name && (<span style={{color:"red"}}>{formik.errors.email}</span>)}
 
-                </div>
+    //             </div>
                 
-                <div className={style.formGroup}>
-                    <label htmlFor="password">Password:</label>
-                    <input onChange={formik.handleChange} type="password" id='password' name='password' value={formik.values.password} required/><br/>
-                    {formik.touched.password && formik.errors.name && (<span style={{color:"red"}}>{formik.errors.password}</span>)}
+    //             <div className={style.formGroup}>
+    //                 <label htmlFor="password">Password:</label>
+    //                 <input onChange={formik.handleChange} type="password" id='password' name='password' value={formik.values.password} required/><br/>
+    //                 {formik.touched.password && formik.errors.name && (<span style={{color:"red"}}>{formik.errors.password}</span>)}
 
-                </div>
+    //             </div>
 
-                <button type='submit'  className = {style.formGroup} >
-                    Register
-                </button>
+    //             <button type='submit'  className = {style.formGroup} >
+    //                 Register
+    //             </button>
 
-            </form>
-    </div>
+    //         </form>
+    // </div>
   );
 }
 
