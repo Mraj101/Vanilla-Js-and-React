e1fd93b19dcccd59ec9a0697ff830e0beee1f738
Hasnain <mrajhasnain101@gmail.com>
2022-11-29 23:15:25 +0600

    fixed home and Todos file

--- reactjsbasics/forms/src/components/Todos.js
+++ reactjsbasics/forms/src/components/Todos.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const Todos = () => {
+const Todos = (props) => {
+    const [todos] = props.todos
+    const todo=todos.map((todo,index)=>{
+        <p> </p>
+})
   return (
     <div>
-        
+      
     </div>
   )
 }
