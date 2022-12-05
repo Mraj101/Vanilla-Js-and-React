03098fdba3b9b80b0c55f2e6d5ff0dba38fd0a83
Hasnain <mrajhasnain101@gmail.com>
2022-12-05 22:16:06 +0600

    tried css disable button did'n work

--- reactjsbasics/my-app/src/component/toggling/toggle.js
+++ reactjsbasics/my-app/src/component/toggling/toggle.js
@@ -4,9 +4,10 @@ const Toggle = () => {
     const[toggle,setToggle]=useState(false)
   return (
     <div className='Container'>
-        {toggle &&   <p className={toggle}>
+        {toggle &&   (<p className={toggle}>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium architecto alias, dicta harum consequuntur tempora modi, ratione ullam quos sint quo esse reprehenderit eligendi voluptate. Ad maiores laboriosam nemo! Velit ullam fuga distinctio illum id, aut laudantium at ut itaque ad excepturi neque hic, cupiditate adipisci, suscipit voluptatum sunt repellendus.
-        </p>}
+        </p>)
+        }
        
         <button onClick={()=>{setToggle(!toggle)}}>{toggle?"hide":"show"}</button>
         <span><h1>
