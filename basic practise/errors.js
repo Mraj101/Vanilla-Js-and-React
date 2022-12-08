c25a6a589d6357df63cecc37e4f84c6f3105477a
Hasnain <mrajhasnain101@gmail.com>
2022-12-08 22:55:29 +0600

    custom errors made

--- basic practise/errors.js
+++ basic practise/errors.js
@@ -1,18 +1,22 @@
 
-var x;
 const inputID = document.querySelector('#number')
 const btn=document.querySelector('#btn')
+const normalDiv=document.querySelector('.div')
 btn.addEventListener('click',()=>{
-    x= inputID.value
-    console.log(`clicked ${x}`)
+    console.log(`clicked `)
+
+    const para = document.createElement('p');
+    var x=inputID.value
    
 try{
     if(x<5)
-        throw "too low"
+        throw "Custom too low-Error"
     else if(x>10)
-        throw "too high"
+        throw "Custom too high-Error"
 }catch(err){
-    console.log(err)
+    para.textContent = err
+    normalDiv.appendChild(para)
+    inputID.value=""
 }finally{
     console.log("here without x")
 }
@@ -20,18 +24,18 @@ try{
 })
 
 
-try{
-console.log('hello');
-printf("hello in c");
+// try{
+// console.log('hello');
+// printf("hello in c");
 
-}catch(err){
-    console.log(` err is : ${err.messege}`)
-}finally{
-    console.log("this thing is a must run")
-}
+// }catch(err){
+//     console.log(` err is : ${err.messege}`)
+// }finally{
+//     console.log("this thing is a must run")
+// }
 
 
 
-console.log(x)// x is not defined cause it will run before everything because of asynchronus programming
+//  x is not defined cause it will run before everything because of asynchronus programming
 
 
