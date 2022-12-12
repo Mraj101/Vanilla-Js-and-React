ff1c7e4f63b315b1ec7db0b43671bfbc825c37af
Hasnain <mrajhasnain101@gmail.com>
2022-12-12 19:23:39 +0600

    xmlHttpRequest api

--- basic practise/api/xmlHttp.js
+++ basic practise/api/xmlHttp.js
@@ -1,25 +1,48 @@
 
 
-const makeRequest=(method,url)=>{
+const makeRequest=(method,url,data)=>{
     const xhr = new XMLHttpRequest();
     xhr.open(method,url);
 
-    xhr.onlaod= ()=>{
+    xhr.onload= ()=>{
             let data= xhr.response;
             console.log(JSON.parse(data));
     }
+    xhr.setRequestHeader('Content-Type',"application/json")
+
     xhr.onerror= ()=>{
         console.log("Error here")
     }
 
-    xhr.send()
+    xhr.send(JSON.stringify(data))
 }
 
 
 
 const getData=()=>{
+ 
     makeRequest("GET","https://jsonplaceholder.typicode.com/posts")
+}
+const postData=()=>{
+    makeRequest("POST","https://jsonplaceholder.typicode.com/posts", {title: 'foo',
+    body: 'bar',
+    userId: 1,
+})
+}
+
+const updateData=()=>{
+    makeRequest("PUT","https://jsonplaceholder.typicode.com/posts/1", {title: 'food khabo',
+    body: 'barma jabo',
+    userId: 1,
+})
+
+}
+const delteData=()=>{
+    makeRequest("DELETE","https://jsonplaceholder.typicode.com/posts/1")
 }
 
 
 getData()
+postData()
+delteData()
+updateData()
