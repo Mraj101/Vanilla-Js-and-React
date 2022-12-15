1a7d9decb204c1ccc599111f276de0a73e86e4b5
Hasnain <mrajhasnain101@gmail.com>
2022-12-15 23:59:31 +0600

    axios api

--- basic practise/api/axios.js
+++ basic practise/api/axios.js
@@ -29,4 +29,22 @@ postData=()=>{
     })
     .then((res)=>console.log(res))
 }
+
+updateData=()=>{
+    makeRequest({
+        url:"https://jsonplaceholder.typicode.com/posts",
+        method:'PUT',
+        data:JSON.stringify({
+            id:1,
+            title:"fooma",
+            body:"barma",
+            userId:1
+        })
+    })
+    .then((res)=>console.log(res.data))
+    .catch((err)=>{
+        console.log(err.message)
+    })
+}
 postData()
+updateData()
