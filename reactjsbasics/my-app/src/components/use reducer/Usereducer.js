661afeff9ddcf3c1ab0bb35bc5491dfae9bce8ee
Hasnain <mrajhasnain101@gmail.com>
2022-12-25 16:12:20 +0600

    more on use reduce

--- reactjsbasics/my-app/src/components/use reducer/Usereducer.js
+++ reactjsbasics/my-app/src/components/use reducer/Usereducer.js
@@ -1,4 +1,4 @@
-import React ,{useState} from 'react'
+import React ,{useState,useReducer} from 'react'
 
 const bookData=[{
    id:1,
@@ -15,30 +15,51 @@ const bookData=[{
 {
     id:4,
     name:"Fahim"
-},
+}
 ]
+
+const Modal=({modalText})=>{
+    return <p>{modalText}</p>
+}
 
 const Usereducer = () => {
  const [books, setBooks]=useState(bookData)
  const [bookName,setBookName]=useState('')
+ const [isModal,setIsModal]=useState(false)
+ const [modalText,setModalText]=useState('')
+
+ const [state, dispatch] = useReducer(reducer,{
+
+ })
+
  const handleSubmit=(e)=>{
     e.preventDefault()
     setBooks((prevBooks)=>{
         const newBook={id:new Date().getTime().toString(), name:bookName}
-        return [...books,newBook]
+        return [...prevBooks,newBook]
     })
+    setIsModal(true)
+    setModalText('Book is added')
  }
+
   return (
     <div className='Container'>
+        <h1>Understanding UseReducer</h1>
         <form onSubmit={handleSubmit}>
             <label htmlFor="text">Name:</label>
             <input type="text" id="Name" value={bookName} onChange={(e)=>setBookName(e.target.value)}/>
             <button type='submit'>Add Name:</button>
         </form>
+        {isModal && <Modal modalText={modalText}/>}
         {books.map((book)=>{
-            return <li key={book.id}>
-                {book.name}
-            </li>
+            return <div>
+                     <li style={{margin:"10px",padding:"5px" ,backgroundColor:"red",color:"white"}} key={book.id}>
+                         {book.name}
+                         <button style={{padding:"5px",marginLeft:"10px" ,backgroundColor:"brown",color:"white"}} type='submit'>remove</button>
+                     </li>
+            </div>
+                    
+                     
         })}
 
     </div>
