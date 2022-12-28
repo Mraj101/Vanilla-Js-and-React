eee7377c8871725c284b056facb29f8ac80a8700
Hasnain <mrajhasnain101@gmail.com>
2022-12-28 22:33:42 +0600

    react router dom setup

---
+++ reactjsbasics/my-app/src/components/React Router Dom/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Home Page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo dolorem sint nostrum enim aliquam repudiandae sit. Libero, officia dicta? Architecto similique quasi suscipit, illum fuga natus. Esse harum eum dolores deserunt vel sunt facere earum, illo magnam id cumque sapiente dicta, perspiciatis porro consequatur qui impedit nostrum sed quidem est.</p>
+        <br />
+        <br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti necessitatibus unde quibusdam saepe vel sed minima placeat optio, eum neque enim ratione labore aperiam omnis, at eos quia tempora ipsa!</p>
+    </div>
+  )
+}
+
+export default Home
