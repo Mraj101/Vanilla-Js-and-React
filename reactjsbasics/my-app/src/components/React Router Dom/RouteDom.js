15710a639aa0772870c19fb102c6a4544288f944
Hasnain <mrajhasnain101@gmail.com>
2022-12-29 21:28:08 +0600

    route dom

--- reactjsbasics/my-app/src/components/React Router Dom/RouteDom.js
+++ reactjsbasics/my-app/src/components/React Router Dom/RouteDom.js
@@ -1,13 +1,40 @@
 import React from 'react'
 import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import { About } from './About'
+import Contact from './Contact'
 
 import Home from './HomePage'
 
 const RouteDom = () => {
   return (
          <BrowserRouter>
+            <div className='Container'>
+              {/* <nav>
+                <ul>
+
+          
+                    <a href="/home">Home</a><br />
+             
+/////////page refreshes and makes process slow why we use Link from react-router-dom
+             
+                    <a href="/contact">contact</a><br />
+                 
+                  
+                 
+                    <a href="/about">contact</a><br />
+                  
+
+                </ul>
+              </nav> */}
+                <h1>Router Dom here... </h1>
+                <h3>add /about in url</h3>
+                <h3>add /contact in url</h3>
+                <h3>add /home in url</h3>
+            </div>
           <Routes>
             <Route path="/home" element={<Home/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/about" element={<About/>}/>
           </Routes>
         </BrowserRouter>
   )
